c677952f4ab4be613b77e28b40fa4abd95705d4b
Pontus Persson <persson@flaskpost.org>
2011-11-23 13:22:14 +0100

    Lagt till egenskaper i car.cs

--- Bilspelet/Bilspelet/Car.cs
+++ Bilspelet/Bilspelet/Car.cs
@@ -13,19 +13,70 @@ namespace Bilspelet
     class Car
     {
         private float wheels;
-        private int HP;
         private float angle;
-        private bool onroad;
-        private int x;
-        private int y;
         private float speedx;
         private float speedy;
+        private int hp;
+        private int x;
+        private int y;
+        private bool onroad;
         private SoundEffect driftsound;
-        private SoundEffect enginesound;
         private Texture2D texture;
 
-        public float Wheels{
-            get{return wheels;}
-            set{
+        public float Wheels
+        {
+            get { return wheels; }
+            set { wheels = value; }
+        }
+        public float Angle
+        {
+            get { return angle; }
+            set { angle = value; }
+        }
+        public float SpeedX
+        {
+            get { return speedx; }
+            set { speedx = value; }
+        }
+        public float SpeedY
+        {
+            get { return speedy; }
+            set { speedy = value; }
+        }
+        public int HP
+        {
+            get { return hp; }
+        }
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public bool OnRoad
+        {
+            get { return onroad; }
+            set { onroad = value; }
+        }
+        public Rectangle Collision
+        {
+            get { return new Rectangle(x, y, texture.Width, texture.Height); }
+        }
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+        public SoundEffect DriftSound
+        {
+            get { return driftsound; }
+            set { driftsound = value; }
+        }
+
     }
 }
