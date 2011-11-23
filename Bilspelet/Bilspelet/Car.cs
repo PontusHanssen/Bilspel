c753e12fcb3151d3309afe16fe7943209e9dd5f4
Pontus Persson <persson@flaskpost.org>
2011-11-23 10:56:00 +0100

    First commit

---
+++ Bilspelet/Bilspelet/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace Bilspelet
+{
+    class Car
+    {
+        private float wheels;
+        private int HP;
+        private float angle;
+        private bool onroad;
+        private int x;
+        private int y;
+        private float speedx;
+        private float speedy;
+        private SoundEffect driftsound;
+        private SoundEffect enginesound;
+        private Texture2D texture;
+
+        public float Wheels{
+            get{return wheels;}
+            set{
+    }
+}
---
+++ Bilspelet/Bilspelet/Physics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bilspelet
+{
+    class Physics
+    {
+        /*
+         * Metoder att koda:
+         * drift(Vector2 speed, int radie, float friktion)
+         * TopSpeed(int HP, float friktion)
+    }
+}
